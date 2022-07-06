7fd29db234405f18d144048f00b17d3966486791
Aleksey Grin <iamdasuper@gmail.com>
2022-07-06 09:51:31 +0300

    therd lesson

--- src/main/java/com/example/sweater/domain/Message.java
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -8,11 +8,19 @@ import javax.persistence.Id;
 @Entity
 public class Message {
     @Id
-    @GeneratedValue(strategy= GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private int id;
 
     private String text;
     private String tag;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag) {
+        this.text = text;
+        this.tag = tag;
+    }
 
     public int getId() {
         return id;
